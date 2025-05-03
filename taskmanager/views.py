f1faf363e359ec240665a502a377040f9a6d2e23
Jayasree Pallavali <jayasri85450@gmail.com>
2025-05-03 17:15:27 +0530

    Created view with filter

--- taskmanager/views.py
+++ taskmanager/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import viewsets, filters
+from .models import Task
+from .serializers import TaskSerializer
+from django_filters.rest_framework import DjangoFilterBackend
 
-# Create your views here.
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    filter_backends = [filters.SearchFilter, DjangoFilterBackend, filters.OrderingFilter]
+    search_fields = ['title']
+    filterset_fields = ['date']
+    ordering_fields = ['date']
