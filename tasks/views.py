6395986f711119a4db26c94d9fdcb53e93d154c2
Jayasree Pallavali <jayasri85450@gmail.com>
2025-05-03 17:14:02 +0530

    Removed tasks app,Since I have created a new app named 'taskmanager'

--- tasks/views.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import viewsets
-from .serializer import TaskSerialize
-from .models import Task
-
-
-class TaskView(viewsets.ModelViewSet):
-    # Create your views here.
-    pass
